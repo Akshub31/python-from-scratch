f69b9c9e60468f1a53b37f11aa4d7373a0d9c133
Akshada Bhangale <88818638+Akshub31@users.noreply.github.com>
2026-08-15 16:08:07 +0200

    Create 09_tuples_sets.py

---
+++ 09_tuples_sets.py
@@ -0,0 +1,25 @@
+# Tuple
+
+coordinates = (10, 20)
+
+print(coordinates[0])
+
+# Set
+
+numbers = {1, 2, 3, 3, 3}
+
+print(numbers)
+
+numbers.add(4)
+numbers.remove(2)
+
+print(numbers)
+
+# Set operations
+
+a = {1, 2, 3}
+b = {3, 4, 5}
+
+print(a | b)  # union
+print(a & b)  # intersection
+print(a - b)  # difference
