3a46d3bf54805511c29d1775a0fb946ae690a0ee
Akshada Bhangale <88818638+Akshub31@users.noreply.github.com>
2026-08-15 16:03:45 +0200

    Create 02_variables.py

---
+++ 02_variables.py
@@ -0,0 +1,23 @@
+name = "Alex"
+age = 20
+height = 1.75
+is_student = True
+
+print(name)
+print(age)
+print(height)
+print(is_student)
+
+# Change a variable
+age = 21
+
+# Multiple variables
+x, y, z = 1, 2, 3
+
+print(x, y, z)
+
+# Check type
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(is_student))
