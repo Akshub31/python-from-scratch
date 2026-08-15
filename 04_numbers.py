b8699c1efebbb6ce2624bfa7f6b7ea8d8d376a1c
Akshada Bhangale <88818638+Akshub31@users.noreply.github.com>
2026-08-15 16:05:21 +0200

    Create 04_numbers.py

---
+++ 04_numbers.py
@@ -0,0 +1,19 @@
+a = 10
+b = 3
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a // b)
+print(a % b)
+print(a ** b)
+
+# Conversion
+x = "100"
+print(int(x))
+
+y = "3.14"
+print(float(y))
+
+print(str(123))
