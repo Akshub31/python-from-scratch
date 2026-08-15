ce5fb787adabfa25974577189be293b56a223708
Akshada Bhangale <88818638+Akshub31@users.noreply.github.com>
2026-08-15 16:06:54 +0200

    Create 07_loops.py

---
+++ 07_loops.py
@@ -0,0 +1,29 @@
+# For loop
+
+for i in range(5):
+    print(i)
+
+
+# While loop
+
+count = 0
+
+while count < 5:
+    print(count)
+    count += 1
+
+
+# Break
+
+for i in range(10):
+    if i == 5:
+        break
+    print(i)
+
+
+# Continue
+
+for i in range(10):
+    if i == 5:
+        continue
+    print(i)
