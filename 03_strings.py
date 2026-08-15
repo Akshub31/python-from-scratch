4f4f9a37bba3034ada6c6bffa8e246529f0932f3
Akshada Bhangale <88818638+Akshub31@users.noreply.github.com>
2026-08-15 16:04:36 +0200

    Create 03_strings.py

---
+++ 03_strings.py
@@ -0,0 +1,25 @@
+name = "Python"
+
+print(name)
+print(len(name))
+print(name.upper())
+print(name.lower())
+print(name.capitalize())
+
+# Indexing
+print(name[0])
+print(name[-1])
+
+# Slicing
+print(name[0:3])
+print(name[:3])
+print(name[3:])
+
+# Replace
+text = "I like Java"
+text = text.replace("Java", "Python")
+print(text)
+
+# f-string
+age = 20
+print(f"I am {age} years old.")
