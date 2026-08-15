68c6f41425860b6f43a4589850b9dec615d9cc68
Akshada Bhangale <88818638+Akshub31@users.noreply.github.com>
2026-08-15 16:02:13 +0200

    Create 01_basics.py

---
+++ 01_basics.py
@@ -0,0 +1,18 @@
+# Print
+print("Hello, Python!")
+
+# Comment
+# This is a comment
+
+# Multiple lines
+print("Hello")
+print("World")
+
+# Basic operators
+print(10 + 5)
+print(10 - 5)
+print(10 * 5)
+print(10 / 5)
+print(10 // 3)
+print(10 % 3)
+print(2 ** 3)
